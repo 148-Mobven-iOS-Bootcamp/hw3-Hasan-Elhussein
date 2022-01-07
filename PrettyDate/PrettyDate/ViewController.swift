ba506da18b27bd592fb7014874cecb6c505802dd
Hasan-Elhussein <hasan.elhussein.97@gmail.com>
2022-01-07 22:44:38 +0300

    Bug fixes and refinments

--- PrettyDate/PrettyDate/ViewController.swift
+++ PrettyDate/PrettyDate/ViewController.swift
@@ -20,15 +20,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        // Set the date format to dd<>mm<>yyyy
-        dateFormatter.dateFormat = "dd<>mm<>yyyy"
     }
     
     
     // MARK: - IBActions
     @IBAction func datePickerValueChanged(_ sender: UIDatePicker) {
         // Set dateLabel to the date taken from UIDatePicker
-        dateLabel.text = dateFormatter.string(from: sender.date)
+        dateLabel.text = sender.date.prettyDate
         
     }
     
