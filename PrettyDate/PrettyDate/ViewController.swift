551e302d4d2ba7e2d565097bfdd6fc1c47bf2ccf
Hasan-Elhussein <hasan.elhussein.97@gmail.com>
2022-01-07 22:42:43 +0300

    Add DateExtension

--- PrettyDate/PrettyDate/ViewController.swift
+++ PrettyDate/PrettyDate/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // IBOutlets
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    // Variables
+    let dateFormatter = DateFormatter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Set the date format to dd<>mm<>yyyy
+        dateFormatter.dateFormat = "dd<>mm<>yyyy"
     }
-
-
+    
+    
+    // MARK: - IBActions
+    @IBAction func datePickerValueChanged(_ sender: UIDatePicker) {
+        // Set dateLabel to the date taken from UIDatePicker
+        dateLabel.text = dateFormatter.string(from: sender.date)
+        
+    }
+    
 }
 
